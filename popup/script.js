ada14566f242c055fb203ee25136615543dd8596
Mike <mike.eirich@gmx.de>
2018-10-04 16:44:29 +0200

    Information of plugins on forums.x-plane.org can be saved in a popup

--- popup/script.js
+++ popup/script.js
@@ -1,38 +1,110 @@
 var regex = /<script type='application\/ld\+json'>\n({\s+"@context": "http:\/\/schema\.org",\s+"@type": "WebApplication",[\s\S]+?)<\/script>/g;
+var pluginName;
+var version;
+var dateString;
+var plugins = [];
 
-var month = new Array();
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
+// function onCleared() {
+//   console.log("OK");
+// }
 
-function httpGet(theUrl){
+// function onError(e) {
+//   console.log(e);
+// }
+
+// var clearStorage = browser.storage.local.clear();
+// clearStorage.then(onCleared, onError);
+
+var month = ["January","February","March","April","May","June","July","August","September","Octobre","November","December"];
+
+getStorage().then(function(){
+	updateDivs(plugins);
+	console.log("loaded!");	
+});
+
+function httpGet(url){
 	let xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", theUrl, true);
+	xmlHttp.open("GET", url, true);
 	xmlHttp.onreadystatechange = function () {
 		if (xmlHttp.readyState != 4) return;
 
 		var response = xmlHttp.responseText;
 		var arr = regex.exec(response);
 		var date = new Date(JSON.parse(arr[1]).dateModified);
-		var version = JSON.parse(arr[1]).softwareVersion;
-		var dateString = date.getDate() + "." + month[date.getMonth()] + "." + date.getFullYear();
-		console.log(dateString+" , version: " + version);
+		version = JSON.parse(arr[1]).softwareVersion;
+		pluginName = JSON.parse(arr[1]).name;
+		dateString = date.getDate() + "." + month[date.getMonth()] + "." + date.getFullYear();
+
+		saveStorage(url,pluginName,version);
+
+		console.log(dateString+" , version: " + version);;
 	};
 	xmlHttp.send();
 }
 
-function convertDate(date){
-
+function getStorage(){
+	return browser.storage.local.get("plugins").then(function(storage) {
+		if(storage.plugins){
+			plugins = storage.plugins;
+			console.log("plugins", plugins);
+		}
+	});
 }
 
-httpGet("https://forums.x-plane.org/index.php?/files/file/41411-fse/");
-// httpGet("https://forums.x-plane.org/index.php?/files/file/46465-speedy-copilot-320-for-a320-ultimate/");
+function saveStorage(url,name,version){
+	plugins.push({url: url, name: name, version: version});
+
+	browser.storage.local.set({
+			"plugins": plugins,
+		});
+	console.log("Saved.")
+
+	getStorage();
+	updateDivs(plugins);		
+}
+
+var addButton = document.getElementById('addButton');
+
+if(addButton){
+	addButton.addEventListener("click", function() {
+		getTabId();
+	});
+}
+
+function getTabId(){
+	function logTabs(tabs) {
+	  for (let tab of tabs) {
+	    httpGet(tab.url);
+	  }
+	}
+
+	function onError(error) {
+	  console.log(`Error: ${error}`);
+	}
+
+	var querying = browser.tabs.query({currentWindow: true, active: true});
+	querying.then(logTabs, onError);
+}
+
+function updateDivs(plugins){
+	var main = document.getElementById("main");
+	while(main.firstChild){
+		main.removeChild(main.firstChild)
+	}
+
+	for (var i = 0; i < plugins.length; i++) {
+		var div = document.createElement("div");
+		div.style.width = "100%";
+		div.style.height = "30px";
+		div.style.background = "#5c5757";
+		div.style.color = "white";
+		div.style.border = "1px solid #363434";
+		div.style.lineHeight = "30px";
+		div.innerHTML = "Plugin: " + plugins[i].name + "Version: " + plugins[i].version;
+
+		main.appendChild(div);
+		console.log("updated.")
+	}
+		
+}
+
