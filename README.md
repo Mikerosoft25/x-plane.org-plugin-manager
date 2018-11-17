14eaf9f877e767fd8a9ad0074440eb4fb1334ed6
Mike <mike.eirich@gmx.de>
2018-11-17 20:00:10 +0100

    add loading indicator when adding plugins, bug fixes & code improvements

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # X-Plane.org Plugin Manager
 
+Get the add-on from the [Mozilla add-on page](https://addons.mozilla.org/de/firefox/addon/x-plane-plugin-manager/).
+
 Having a lot of X-Plane addons can be confusing sometimes and hard to manage. This plugin-manager allows you to add plugins from [forums.x-plane](https://forums.x-plane.org/) into a popup, where crucial plugin information as the name, installed version and current version will be shown.  This creates a simple overview over all plugins and informs users of new updates of added plugins.
 
 This Add-on is still work in progress and currently **only** works in Firefox.
--- popup/script.js
+++ popup/script.js
@@ -50,6 +50,7 @@ function httpGet(url, callback){
 		var arr = regex.exec(response);
 
 		if(!arr) {
+			removeSpinner();
 			toast("Something went wrong!");
 		}	
 
@@ -70,9 +71,10 @@ var addButton = document.getElementById('addDiv');
 
 if(addButton){
 	addButton.addEventListener("click", function() {
+		addSpinner();
 		getTabId().then(function(url) {
-			toast("Adding plugin to list...");
 			httpGet(url, function(obj) {
+				document.getElementById('inputField').value = "";
 				if(!checkExists(obj)){
 					plugins.push(obj);
 					curVers[plugins.length-1] = obj.version;
@@ -89,9 +91,11 @@ function checkExists(obj){
 	for (var i = 0; i < plugins.length; i++) {
 		if(plugins[i].name == obj.name){
 			if(plugins[i].version == obj.version){
+				removeSpinner();
 				toast("This plugin is up to date!");
 			}
 			else{
+				removeSpinner();
 				plugins[i].version = obj.version;
 				saveStorage();
 				toast("plugin updated!");
@@ -124,7 +128,6 @@ function populateDivs(plugins, curVers){
 		var insVerDiv = document.createElement("div");
 		var curVerDiv = document.createElement("div");
 
-		
 		wrapper.setAttribute("data-id", plugins[i].id);
 
 		wrapper.classList.add("wrapper");
@@ -159,15 +162,13 @@ function populateDivs(plugins, curVers){
 		var URL = plugins[i].url;
 
 		nameDiv.addEventListener("click", function(){
-			window.open(plugins[this.parentElement.parentElement.getAttribute("data-id")].url);
+			window.open(pluginsTemp[this.parentElement.parentElement.getAttribute("data-id")].url);
 		});
 		
-
 		if(plugins.length >= 12){
 			document.body.style.overflow = "scroll";
 			document.body.style.overflowX = "hidden";
 		}
-
 	}
 	if(showDelImgs){
 		removeImgs();
@@ -219,7 +220,8 @@ function addDiv(plugin, curVersion){
 	
 	var URL = plugin.url;
 	nameDiv.addEventListener("click", function(){
-		window.open(plugins[this.parentElement.parentElement.getAttribute("data-id")].url);
+		window.open(pluginsTemp[this.parentElement.parentElement.getAttribute("data-id")].url);
+		// console.log(this.parentElement.parentElement.getAttribute("data-id");
 	});
 		
 
@@ -231,18 +233,21 @@ function addDiv(plugin, curVersion){
 
 function updateDiv(name, insVer, curVer, id){
 	var divs = document.querySelectorAll(".wrapper");
-	var nameDiv = divs[id].children[0].children[0];
-	var imgDiv = divs[id].children[0].children[1];
-	var insVerDiv = divs[id].children[1].children[0];
-	var curVerDiv = divs[id].children[1].children[1];
+	if(divs[id]){
+		var nameDiv = divs[id].children[0].children[0];
+		var imgDiv = divs[id].children[0].children[1];
+		var insVerDiv = divs[id].children[1].children[0];
+		var curVerDiv = divs[id].children[1].children[1];
 
-	nameDiv.innerText = name;
-	insVerDiv.innerText = insVer;
-	curVerDiv.innerText = curVer;
-	imgDiv.innerText = "";
-	checkVers(id);
-	if (showDelImgs) {
-		addDelImgs();
+		nameDiv.innerText = name;
+		insVerDiv.innerText = insVer;
+		curVerDiv.innerText = curVer;
+		imgDiv.innerText = "";
+		checkVers(id);
+		if (showDelImgs) {
+			removeImgs();
+			addDelImgs();
+		}
 	}
 }
 
@@ -255,9 +260,13 @@ if(refreshButton){
 }
 
 function getCurrentVersions(){
-
 	var imgDivs = document.querySelectorAll(".imgDiv");
+	var input = document.getElementById('inputField');
 	removeImgs();
+
+	var editImg = document.getElementById("editImg");
+	editImg.setAttribute("src", "../icons/edit.svg");
+	toggle = true;
 
 	for (let i = 0; i < plugins.length; i++) {
 		loading(imgDivs[i]);
@@ -265,6 +274,9 @@ function getCurrentVersions(){
 			curVers[i] = obj.version;
 
 			updateDiv(plugins[i].name, plugins[i].version, curVers[i], i);
+			if(input.value.length > 0){
+				getSearch();	
+			}
 		});
 	}
 	curVersTemp = curVers.slice();	
@@ -290,11 +302,10 @@ if(editButton){
 			editImg.setAttribute("src", "../icons/edit.svg");
 			toggle = true;
 			showDelImgs = false;
-			// getCurrentVersions();
 			for (var i = 0; i < plugins.length; i++) {
 				checkVers(i);
 			}
-		}
+		}		
 	});
 }
 
@@ -302,18 +313,20 @@ var input = document.getElementById('inputField');
 
 if(input){
 	input.addEventListener("input", function(){
-		var inputText = input.value;
-		getSearch(inputText);
+		getSearch();
 		pluginsTemp = plugins.slice();
 		curVersTemp = curVers.slice();
 	});	
 }
 
-function getSearch(input){
+function getSearch(){
+	var input = document.getElementById('inputField');
+	var inputText = input.value;
+
 	var plugArr = [];
 	var verArr = [];
 	for (var i = 0; i < plugins.length; i++) {
-		if(plugins[i].name.toLowerCase().includes(input)){
+		if(plugins[i].name.toLowerCase().includes(inputText)){
 			plugArr.push(plugins[i]);
 			verArr.push(curVers[i]);
 		}
@@ -330,7 +343,6 @@ function getSearch(input){
 
 function addDelImgs(){
 	var imgDivs = document.querySelectorAll(".imgDiv");
-	var input = document.getElementById('inputField');
 
 	for (var i = 0; i < imgDivs.length; i++) {
 		var deleteImg = document.createElement("img");
@@ -348,7 +360,7 @@ function addDelImgs(){
 			pluginsTemp.splice(this.parentElement.parentElement.parentElement.getAttribute("data-id"), 1);
 
 			saveStorage();
-			getSearch(input.value);
+			getSearch();
 		}); 
 	}	
 }
@@ -363,47 +375,49 @@ function removeImgs(){
 }
 
 function loading(parent){
-	var circle = document.createElement("div");
-	circle.classList.add("sk-fading-circle", "circle");
-	var circ1 = document.createElement("div");
-	circ1.classList.add("sk-circle1", "sk-circle");
-	var circ2 = document.createElement("div");
-	circ2.classList.add("sk-circle2", "sk-circle");
-	var circ3 = document.createElement("div");
-	circ3.classList.add("sk-circle3", "sk-circle");
-	var circ4 = document.createElement("div");
-	circ4.classList.add("sk-circle4", "sk-circle");
-	var circ5 = document.createElement("div");
-	circ5.classList.add("sk-circle5", "sk-circle");
-	var circ6 = document.createElement("div");
-	circ6.classList.add("sk-circle6", "sk-circle");
-	var circ7 = document.createElement("div");
-	circ7.classList.add("sk-circle7", "sk-circle");
-	var circ8 = document.createElement("div");
-	circ8.classList.add("sk-circle8", "sk-circle");
-	var circ9 = document.createElement("div");
-	circ9.classList.add("sk-circle9", "sk-circle");
-	var circ10 = document.createElement("div");
-	circ10.classList.add("sk-circle10", "sk-circle");
-	var circ11 = document.createElement("div");
-	circ11.classList.add("sk-circle11", "sk-circle");
-	var circ12 = document.createElement("div");
-	circ12.classList.add("sk-circle12", "sk-circle");
+	if(parent){
+		var circle = document.createElement("div");
+		circle.classList.add("sk-fading-circle", "circle");
+		var circ1 = document.createElement("div");
+		circ1.classList.add("sk-circle1", "sk-circle");
+		var circ2 = document.createElement("div");
+		circ2.classList.add("sk-circle2", "sk-circle");
+		var circ3 = document.createElement("div");
+		circ3.classList.add("sk-circle3", "sk-circle");
+		var circ4 = document.createElement("div");
+		circ4.classList.add("sk-circle4", "sk-circle");
+		var circ5 = document.createElement("div");
+		circ5.classList.add("sk-circle5", "sk-circle");
+		var circ6 = document.createElement("div");
+		circ6.classList.add("sk-circle6", "sk-circle");
+		var circ7 = document.createElement("div");
+		circ7.classList.add("sk-circle7", "sk-circle");
+		var circ8 = document.createElement("div");
+		circ8.classList.add("sk-circle8", "sk-circle");
+		var circ9 = document.createElement("div");
+		circ9.classList.add("sk-circle9", "sk-circle");
+		var circ10 = document.createElement("div");
+		circ10.classList.add("sk-circle10", "sk-circle");
+		var circ11 = document.createElement("div");
+		circ11.classList.add("sk-circle11", "sk-circle");
+		var circ12 = document.createElement("div");
+		circ12.classList.add("sk-circle12", "sk-circle");
 
-	circle.appendChild(circ1);
-	circle.appendChild(circ2);
-	circle.appendChild(circ3);
-	circle.appendChild(circ4);
-	circle.appendChild(circ5);
-	circle.appendChild(circ6);
-	circle.appendChild(circ7);
-	circle.appendChild(circ8);
-	circle.appendChild(circ9);
-	circle.appendChild(circ10);
-	circle.appendChild(circ11);
-	circle.appendChild(circ12);
+		circle.appendChild(circ1);
+		circle.appendChild(circ2);
+		circle.appendChild(circ3);
+		circle.appendChild(circ4);
+		circle.appendChild(circ5);
+		circle.appendChild(circ6);
+		circle.appendChild(circ7);
+		circle.appendChild(circ8);
+		circle.appendChild(circ9);
+		circle.appendChild(circ10);
+		circle.appendChild(circ11);
+		circle.appendChild(circ12);
 
-	parent.appendChild(circle);
+		parent.appendChild(circle);
+	}
 }
 
 function checkVers(id){
@@ -413,13 +427,14 @@ function checkVers(id){
 		var insVerDiv = divs[id].children[1].children[0];
 		var curVerDiv = divs[id].children[1].children[1];
 
-		if(insVerDiv.innerText === curVerDiv.innerText){
+		if(insVerDiv.innerText === curVerDiv.innerText && toggle){
+			
 			var goodImg = document.createElement("img");
 			goodImg.setAttribute("src", "../icons/good.svg");
 			goodImg.classList.add("goodImgs");
 			imgDiv.appendChild(goodImg);				
 		}
-		else if(insVerDiv.innerText != curVerDiv.innerText && curVers[id]){
+		else if(insVerDiv.innerText != curVerDiv.innerText && curVers[id] && toggle){
 			var errorImg = document.createElement("img");
 			errorImg.setAttribute("src", "../icons/error.svg");
 			errorImg.classList.add("errorImg");
@@ -435,4 +450,34 @@ function toast(text){
     toast.innerText = text;
 
     setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
+}
+
+function addSpinner(){
+	var spinner = document.querySelectorAll(".spinner")[0];
+
+	if(!spinner){
+		var main = document.getElementById("main");
+
+		var spinner = document.createElement("div");
+		var bounce1 = document.createElement("div");
+		var bounce2 = document.createElement("div");
+		var bounce3 = document.createElement("div");
+
+		spinner.classList.add("spinner");
+		bounce1.classList.add("bounce1");
+		bounce2.classList.add("bounce2");
+		bounce3.classList.add("bounce3");
+
+		spinner.appendChild(bounce1);
+		spinner.appendChild(bounce2);
+		spinner.appendChild(bounce3);
+		main.appendChild(spinner);
+	}
+}
+
+function removeSpinner(){
+	var spinner = document.querySelectorAll(".spinner")[0];
+	if(spinner){
+		spinner.parentNode.removeChild(spinner);
+	}
 }
